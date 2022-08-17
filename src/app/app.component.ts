496b19aa741e8e029cae05b029732bf786f009d5
rdwayne876 <63379431+rdwayne876@users.noreply.github.com>
2022-08-16 19:34:43 -0500

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+ 
 export class AppComponent {
   title = 'angular-list';
+
+  // @ViewChild(MatSort) sort: MatSort
+
+  
+
+  itemForm!: FormGroup
+
+  items: any[] = []
+  total: number = 0
+
+  displayedColumns: string[] = [ 'Name', 'Quantity', 'Price', 'Total']
+  dataSource = this.items
+
+  constructor( private formBuilder: FormBuilder){}
+
+  ngOnInit(): void {
+    this.itemForm = this.formBuilder.group({
+      Name: [ '', Validators.required],
+      Quantity: [ '', Validators.required],
+      Price: [ '', Validators.required]
+    })
+  }
+
+  ngAfterViewInit(){
+  }
+
+  addItem() {
+    if( this.itemForm.valid){
+      // this.items.push(this.itemForm.value)
+      console.log(this.items);
+    }
+    this.onAdd()
+  }
+
+  onAdd(){
+    let model = this.itemForm.value
+    this.dataSource.push(model)
+    this.dataSource = [...this.dataSource]
+    this.dataSource.sort(function( a, b){
+      let x = a.Name.toLowerCase()
+      let y = b.Name.toLowerCase()
+      if( x < y) { return -1}
+      if( x > y) { return 1}
+      return 0
+    })
+    this.getTotal()
+  }
+
+  getTotal() {
+    this.total = this.dataSource.reduce(( accumulator, obj) =>  accumulator + ( obj.Price * obj.Quantity), 0)
+    console.log(this.total);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +20,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
